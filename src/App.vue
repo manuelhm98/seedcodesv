9c5a315b9c8e05296978a6fffa1850c7855709bd
danniel-contreras <contrerasdannniel@gmail.com>
2023-06-27 16:20:37 -0600

    Firt page

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen">
+    <div
+      class="bg-[url('assets/banner.svg')] w-screen h-[700px] bg-cover bg-no-repeat"
+    >
+      <div class="w-screen h-[700px] flex flex-col justify-center items-center">
+        <p class="text-3xl text-white font-bold">SEEDCODE SV</p>
+        <p class="text-3xl text-white font-bold">EN CONSTRUCCIÓN</p>
+        <img class="mt-3 w-24 sm:w-32 md:w-44 lg:w-64" src="./assets/loading.gif" alt="">
+      </div>
+    </div>
+  </div>
+</template>
